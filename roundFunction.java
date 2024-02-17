2bfe53ae27bbe8bf2d8aec3c9df3b79cab2ec4db
ayaan <107512637+ayaannshaikhh@users.noreply.github.com>
2024-02-16 22:09:19 -0500

    Add files via upload

---
+++ roundFunction.java
@@ -0,0 +1,17 @@
+package roundFunction;
+
+import java.util.Scanner;
+
+public class RoundMe {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("+ ROUND CALCULATOR +\nUse this tool to round any integer with a decimal spot!");
+		double userInput = scanner.nextDouble();
+		
+		int result =(int)(userInput + 0.5);
+		
+		System.out.println("The original input, " + userInput + " has been rounded to: " + result + ".");
+	}
+
+}
